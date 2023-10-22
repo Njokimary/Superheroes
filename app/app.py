82f2347d777fe6cecfa06e0b5064baff56b93b83
Njokimary <marynjoki738@gmail.com>
2023-10-22 17:12:32 +0300

    feat:create models,get method

---
+++ app/app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response,request,jsonify
+from flask_migrate import Migrate
+from flask_restful import Api,Resource
+
+from models import db, Hero,HeroPower,Power
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+
+api = Api(app)
+
+
+@app.route('/')
+def home():
+    return ''
+
+class Heroes(Resource):
+    def get(self):
+        heroes =Hero.query.all()
+        heroes_list = []
+        for hero in heroes:
+            hero_dict ={
+                "id": hero.id,
+                "name": hero.name,
+                "super_name":hero.super_name
+            }
+            heroes_list.append(hero_dict)
+        return make_response(jsonify(heroes_list),200)
+    
+api.add_resource(Heroes,"/heroes")
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
---
+++ app/models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    super_name = db.Column(db.String(20))
+
+    # Establish a one-to-many relationship from Hero to HeroPower
+    hero_powers = db.relationship('HeroPower', back_populates='hero')
+
+class Power(db.Model,SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    description = db.Column(db.String(20))
+
+    # Establish a one-to-many relationship from Power to HeroPower
+    power_heroes = db.relationship('HeroPower', back_populates='power')
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'heropowers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    
+
+    # Establish many-to-one relationships to Hero and Power
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='power_heroes')
+
+# add any models you may need. 
