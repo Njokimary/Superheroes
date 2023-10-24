b6f603fdb8cfecfec8b296d6e8b331e7c8aca3b2
Njokimary <marynjoki738@gmail.com>
2023-10-24 21:00:14 +0300

    feat:CORS updates

--- app/app.py
+++ app/app.py
@@ -3,16 +3,20 @@
 from flask import Flask, make_response,request,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
+from flask_cors import CORS
 
 from models import db, Hero,HeroPower,Power
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = True
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 
 api = Api(app)
@@ -109,6 +113,50 @@ class PowerById(Resource):
 
 api.add_resource(PowerById, "/powers/<int:id>")
 
+class HeroPowers(Resource):
+    def post(self):
+        data=request.get_json()
+        hero_power=HeroPower( 
+            strength = data["strength"], 
+            power_id= data['power_id'],
+            hero_id= data['hero_id'])
+        
+        try:
+            db.session.add(hero_power)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return make_response(
+                {
+  "errors": ["validation errors"]
+}, 400
+            )
+        
+        hero = Hero.query.get(data['hero_id'])
+
+        if not hero:
+          
+            return make_response({"error": "Associated Hero not found"}, 404)
+
+        powers = []
+        for hero_power in hero.hero_powers:
+            power_dict = {
+                "id": hero_power.power.id,
+                "name": hero_power.power.name,
+                "description": hero_power.power.description
+            }
+            powers.append(power_dict)
+
+        response_data = {
+            "hero_id": hero.id,
+            "hero_name": hero.name,
+            "hero_super_name": hero.super_name,
+            "hero_powers": powers
+        }
+
+        return make_response(jsonify(response_data), 201)
+        
+api.add_resource(HeroPowers, "/hero_powers")
 
 
 
